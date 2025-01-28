e8120140fe23414d0ac9c346dfaa5c996b193752
Theo Leao Larrieux <larrieuxtheo@gmail.com>
2025-01-27 19:19:32 -0600

    Add endpoint to get by chapterId

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,14 @@ import db from './mongoConnect.js';
 const app = express()
 const port = 3000
 
-app.get("/", async (req, res) => {
+app.get("/getChapters", async (req, res) => {
     let results = await db.collection("chapters").find({}).toArray()
+    console.log(results);
+    res.send(results).status(200);
+  });
+
+app.get("/getChapters/:id", async (req, res) => {
+    let results = await db.collection("chapters").findOne({chapterId: req.params.id})
     console.log(results);
     res.send(results).status(200);
   });
